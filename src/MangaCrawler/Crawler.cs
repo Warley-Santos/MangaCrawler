1e8b6cd46b39731e0beb219fd160d8c2c99972cb
Warley-Santos <warley.wagner@hotmail.com>
2020-02-04 22:53:32 -0300

    Classe de testes

--- src/MangaCrawler/Crawler.cs
+++ src/MangaCrawler/Crawler.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MangaCrawler
 {
 	class Crawler
 	{
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine(args.Length);
+
+			var t = Task.Run(() => GetURI(new Uri("https://unionleitor.top/assets/busca.php?q=solo")));
+			t.Wait();
+
+			Console.WriteLine(t.Result);
+			Console.ReadLine();
 		}
+
+		static async Task<string> GetURI(Uri u)
+		{
+			var response = string.Empty;
+			using (var client = new HttpClient())
+			{
+				HttpResponseMessage result = await client.GetAsync(u);
+				if (result.IsSuccessStatusCode)
+				{
+					response = await result.Content.ReadAsStringAsync();
+				}
+			}
+			return response;
+		}
+
 	}
 }
