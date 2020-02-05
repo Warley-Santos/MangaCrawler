61b1c18815fd380b4a6b4ccf9f5759d32d23dd1b
Warley-Santos <warley.wagner@hotmail.com>
2020-02-04 22:52:07 -0300

    Refatoração e novos testes.

--- src/MangaCrawlerTests/MangaTests.cs
+++ src/MangaCrawlerTests/MangaTests.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using MangaCrawler;
 
@@ -6,27 +5,36 @@ namespace MangaCrawlerTests
 {
 	public class MangaTests
 	{
-	
+		private IWebCrawler Crawler;
+
+		public MangaTests()
+		{
+			var source = new UnionMangasSource();
+			Crawler = new UnionMangasCrawler(source);
+		}
+
 		[Fact]
 		public void GetTitulosAscendingTest()
 		{
-			var source = new UnionMangasSource();
-			var crawler = new UnionMangasCrawler(source);
-
-			var titles = crawler.GetMangasAscendingOrder();
+			var titles = Crawler.GetMangasAscendingOrder();
 
 			Assert.NotNull(titles[0].MangaCoverUrl);
 			Assert.NotNull(titles[0].MangaName);
 			Assert.NotNull(titles[0].MangaUrl);
 		}
+		[Fact]
+		public void GetMangasVisualizationOrderTest()
+		{
+			var titles = Crawler.GetMangasVisualizationOrder();
 
+			Assert.NotNull(titles[0].MangaCoverUrl);
+			Assert.NotNull(titles[0].MangaName);
+			Assert.NotNull(titles[0].MangaUrl);
+		}
 		[Fact]
 		public void GetCapitulosUrlTest()
 		{
-			var source = new UnionMangasSource();
-			var crawler = new UnionMangasCrawler(source);
-
-			var chapters = crawler.GetChapters(@"https://unionleitor.top/manga/solo-leveling");
+			var chapters = Crawler.GetChapters(@"https://unionleitor.top/manga/solo-leveling");
 
 			Assert.Equal("Cap. 00", chapters[0].Name);
 			Assert.Equal("26/11/2018", chapters[0].ReleaseDate);
@@ -36,13 +44,20 @@ namespace MangaCrawlerTests
 		[Fact]
 		public void GetPaginasTest()
 		{
-			var source = new UnionMangasSource();
-			var crawler = new UnionMangasCrawler(source);
-
-			var pages = crawler.GetPages(@"http://unionmangas.top/leitor/Solo_Leveling/00");
+			var pages = Crawler.GetPages(@"http://unionmangas.top/leitor/Solo_Leveling/00");
 
 			Assert.NotNull(pages);
 			Assert.NotNull(pages[0].PageUrl);
 		}
+
+		[Fact]
+		public void SearchTest()
+		{
+			var mangas = Crawler.Search("Solo Leveling");
+
+			Assert.Equal("Solo Leveling", mangas[0].Titulo);
+			Assert.Equal("Solo Leveling (Novel)", mangas[1].Titulo);
+		}
+
 	}
 }
