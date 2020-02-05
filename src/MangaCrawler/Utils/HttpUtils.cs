88a65f038ee0c0c129f0fbd1a601aa6d7795cd7d
Warley-Santos <warley.wagner@hotmail.com>
2020-02-04 22:51:34 -0300

    Utils...

---
+++ src/MangaCrawler/Utils/HttpUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaCrawler
+{
+	class HttpUtils
+	{
+		public static async Task<string> GetURI(Uri u)
+		{
+			var response = string.Empty;
+			using (var client = new HttpClient())
+			{
+				HttpResponseMessage result = await client.GetAsync(u);
+				if (result.IsSuccessStatusCode)
+				{
+					response = await result.Content.ReadAsStringAsync();
+				}
+			}
+			return response;
+		}
+	}
+}
